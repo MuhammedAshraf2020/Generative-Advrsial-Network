c0c9a1975b9f4dea51fa099b27edc23890509b7f
MuhammedAsharaf <53389411+MuhammedAshraf2020@users.noreply.github.com>
2021-05-06 06:34:41 +0200

    Update models.py

--- DCGAN/models.py
+++ DCGAN/models.py
@@ -1,50 +1,53 @@
 import torch.nn as nn
 
-class Generator(nn.Module):
-  def __init__(self , z_dim , img_channels , img_size):
-    super(Generator , self).__init__()
-    self.model = nn.Sequential(
-			self.block(z_dim         , img_size*16 , 4 , 1 , 0 ),			
-			self.block(img_size*16   , img_size*8  , 4 , 2 , 1 ),
-			self.block(img_size*8    , img_size*4  , 4 , 2 , 1 ),
-			self.block(img_size*4    , img_size*2  , 4 , 2 , 1 ),
-			nn.ConvTranspose2d(img_size * 2 , img_channels , kernel_size = 4 , stride = 2 , padding = 1) ,
-			nn.Tanh()
-			)						                        
-  
-  def block(self ,in_channels , out_channels  , kernel_size, stride , padding):
-    return nn.Sequential(
-			nn.ConvTranspose2d(in_channels , out_channels , kernel_size , stride , padding , bias = False),
-			nn.BatchNorm2d(out_channels),
-			nn.ReLU()
-			)
-  
-  def forward(self , img):
-    return self.model(img)
-
-
-
 class Discriminator(nn.Module):
-	def __init__(self , img_channels , img_size):
+	def __init__(self , n_channels ,  width):
 		super(Discriminator , self).__init__()
 		self.model = nn.Sequential(
-			nn.Conv2d(img_channels , img_size , kernel_size = 4 , stride = 2 , padding = 1) ,
-			nn.LeakyReLU(0.02) ,
-			self.block(img_size   , img_size*2 , 4 , 2 , 1 ),
-			self.block(img_size*2 , img_size*4 , 4 , 2 , 1 ),
-			self.block(img_size*4 , img_size*8 , 4 , 2 , 1 ),
-			nn.Conv2d(img_size *8 , 1 , kernel_size = 4 , stride = 2 , padding = 0) ,
-			nn.Sigmoid()
-			)
-
-	
-	def block(self , in_channels , out_channels  , kernel_size, stride , padding ):
-		return nn.Sequential(
-			nn.Conv2d(in_channels , out_channels , kernel_size , stride , padding , bias = False) ,
-			nn.BatchNorm2d(out_channels),
-			nn.LeakyReLU(0.2) ,
-
-			)
+			nn.Conv2d(n_channels , width , 4 , 2 , 1 , bias = False) ,
+			nn.LeakyReLU(0.2 , inplace = True) ,
+			#32  , 32 , 64
+			nn.Conv2d(width , width * 2 , 4 , 2 , 1 , bias = False) ,
+			nn.BatchNorm2d(width*2) ,
+			nn.LeakyReLU(0.2 , inplace = True) ,
+			#16  , 16 , 128
+			nn.Conv2d(width * 2 , width * 4 , 4 , 2 , 1 , bias = False) ,
+			nn.BatchNorm2d(width*4) ,
+			nn.LeakyReLU(0.2 , inplace = True) ,
+			#8   , 8  , 265
+			nn.Conv2d(width * 4 , width * 8 , 4 , 2 , 1 , bias = False) ,
+			nn.BatchNorm2d(width*8) ,
+			nn.LeakyReLU(0.2 , inplace = True) ,			
+			#4   , 4  , 512
+			nn.Conv2d(width * 8 , 1 , 4 , 2 , 0 , bias = False) ,
+			nn.Sigmoid())
 
 	def forward(self , x):
-		return self.model(x)
+		return self.model(x).view(x.shape[0] , -1)
+
+
+class Generator(nn.Module):
+  def __init__(self , z_dim , n_channels , width):
+    super(Generator , self).__init__()
+    self.model = nn.Sequential(
+				nn.ConvTranspose2d(z_dim , width * 8 , 4 , 1 , 0 , bias = False) ,
+				nn.BatchNorm2d(width * 8) ,
+				nn.ReLU(True) ,
+				#4 , 4 , 512
+				nn.ConvTranspose2d(width * 8 , width * 4 , 4 , 2 , 1 , bias = False) ,
+				nn.BatchNorm2d(width * 4) ,
+				nn.ReLU(True) ,
+				#8 , 8 , 256
+				nn.ConvTranspose2d(width * 4 , width * 2 , 4 , 2 , 1 , bias = False) ,
+				nn.BatchNorm2d(width * 2) ,
+				nn.ReLU(True) ,
+				#16 , 16 , 128
+				nn.ConvTranspose2d(width * 2 , width  , 4 , 2 , 1 , bias = False) ,
+				nn.BatchNorm2d(width) ,
+				nn.ReLU(True) ,
+				#32 , 32 , 64
+				nn.ConvTranspose2d(width , n_channels , 4 , 2 , 1 , bias = False) ,
+				#64 , 64 , 3
+				nn.Tanh())
+  def forward(self , x):
+			return self.model(x)
