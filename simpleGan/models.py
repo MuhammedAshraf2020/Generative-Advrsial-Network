7790878211ebb7818a7e5875e667a0d556a8e534
MuhammedAsharaf <53389411+MuhammedAshraf2020@users.noreply.github.com>
2021-05-14 08:01:23 +0200

    Update models.py

--- simpleGan/models.py
+++ simpleGan/models.py
@@ -6,17 +6,13 @@ class Discriminator(nn.Module):
   def __init__(self , in_units):
     super(Discriminator , self).__init__()
     self.model = nn.Sequential(
-				*self.disc_block(in_units , 512 ),
-        		*self.disc_block(512 , 128) ,
-				 nn.Linear(128 , 1),
-				 nn.Sigmoid()
+        nn.Linear(in_features = in_units , out_features = 512)
+				nn.LeakyReLU(0.01)
+        nn.Linear(in_features = 512 , out_features = 128)
+				nn.LeakyReLU(0.1)        
+        nn.Linear(128 , 1),
+				nn.Sigmoid()
 			)
-  
-  def disc_block(self , in_units , out_units , normalize = True):
-    layers =  []
-    layers.append(nn.Linear(in_features = in_units , out_features = out_units)) 
-    layers.append(nn.LeakyReLU(0.01))
-    return layers
   
   def forward(self , x):
     return self.model(x)
